0f0782ce6bd65296fa384d6eede9af25a8bc379d
dipeji <dpatidar336@gmail.com>
2020-04-15 19:54:16 +0530

    search screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, View, Text, Button } from 'react-native';
+
+const SearchScreen = () => {
+    return <View>
+        <Text> Search Screen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
